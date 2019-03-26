802c2c07216d0f1205070402b9dc987ce0873315
Raineer <delaritaraineer81@gmail.com>
2019-03-27 01:42:15 +0800

    ContentLoaded DOM ready function()

--- store.js
+++ store.js
@@ -1,23 +1,33 @@
 'use strict';
 
-const removeCartItemButtons = document.getElementsByClassName('btn-danger');
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', ready);
+} else {
+    ready();
+}
+
+function ready() {
+    const removeCartItemButtons = document.getElementsByClassName('btn-danger');
 
  
-for (var i = 0; i < removeCartItemButtons.length; i++) {
-    const button = removeCartItemButtons[i];
-    button.addEventListener('click', function(event) {
-       const buttonCLicked = event.target;
-       buttonCLicked.parentElement.parentElement.remove();
-       updateCartTotal();
-    })
+    for (var i = 0; i < removeCartItemButtons.length; i++) {
+        const button = removeCartItemButtons[i];
+        button.addEventListener('click', function(event) {
+        const buttonCLicked = event.target;
+        buttonCLicked.parentElement.parentElement.remove();
+        updateCartTotal();
+        });
+    }
 }
+
+
 
 function updateCartTotal() {
     const cartItemContainer = document.getElementsByClassName('cart-items')[0];
     const cartRows = cartItemContainer.getElementsByClassName('cart-row');
     let total = 0;
     for (var i = 0; i < cartRows.length; i++) {
-        const cartRow = cartRows[i];
+        const cartRow = cartRows[i];   
         const priceElement = cartRow.getElementsByClassName('cart-price')[0];
         const quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0];
         const price = parseFloat(priceElement.innerText.replace('$', ''));
