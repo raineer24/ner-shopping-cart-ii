8494bc6ae033a2caa942bffacde88dcea6343bab
Raineer <delaritaraineer81@gmail.com>
2019-03-27 00:24:56 +0800

    addeventlistener btn remove()

--- store.js
+++ store.js
@@ -1,1 +1,13 @@
-console.log('here');
+'use strict';
+
+const removeCartItemButtons = document.getElementsByClassName('btn-danger');
+
+ 
+for (var i = 0; i < removeCartItemButtons.length; i++) {
+    const button = removeCartItemButtons[i];
+    button.addEventListener('click', function(event) {
+       const buttonCLicked = event.target;
+       buttonCLicked.parentElement.parentElement.remove();
+    })
+}
+
