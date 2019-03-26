bfe2f2be7e1bc56eff5b7c658bc40432f73edf21
Raineer <delaritaraineer81@gmail.com>
2019-03-27 03:40:30 +0800

    addventlistener and purchaseClicked function code block

--- store.js
+++ store.js
@@ -25,7 +25,13 @@ function ready() {
     for (var i = 0; i < addToCartButtons.length; i++) {
         const button = addToCartButtons[i];
         button.addEventListener('click', addToCartClicked); 
-    }
+    };
+
+    document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchaseClicked);
+}
+
+function purchaseClicked(){
+    
 }
 
 function removeCartItem(event) {
